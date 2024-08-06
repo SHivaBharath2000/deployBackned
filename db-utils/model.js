a82379a1d8b4481eebb06fbd4e4e79167fca5f49
SHivaBharath2000 <152796403+SHivaBharath2000@users.noreply.github.com>
2024-08-06 22:31:25 +0530

    Add files via upload

---
+++ db-utils/model.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+
+const teacherSchema = new mongoose.Schema({
+  id: {
+    type: "string",
+    required: true,
+  },
+  name: {
+    type: "string",
+    required: true,
+  },
+  course: {
+    type: "string",
+    required: true,
+  },
+  batch: {
+    type: "string",
+    required: true,
+  },
+  students: {
+    type: "array",
+    required: true,
+    default: [],
+  },
+});
+
+//Model creation using schema
+const teacherModel = new mongoose.model("teacher", teacherSchema, "teachers");
+//objectname,schema,collection name value
+
+const userSchema = new mongoose.Schema({
+  id: {
+    type: "string",
+    required: true,
+  },
+  name: {
+    type: "string",
+    required: true,
+  },
+  email: {
+    type: "string",
+    required: true,
+  },
+  password: {
+    type: "string",
+    required: true,
+  },
+  role: {
+    type: "string",
+    required: true,
+  },
+  isVerified: {
+    type: "boolean",
+    required: false,
+  },
+});
+
+const userModel = new mongoose.model("user", userSchema, "users");
+export { teacherModel , userModel};
